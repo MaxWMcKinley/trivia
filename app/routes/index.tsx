b81f77debdd9c1d4ee0a36442e76c63364565f20
Ben Ashworth <bashworthj@gmail.com>
2022-08-17 19:54:33 -0400

    deleted files

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -19,38 +19,29 @@ export default function Index() {
             </div>
             <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
               <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
-                <span className="block uppercase px-4 text-white drop-shadow-2xl">
+                <span className="block px-4 uppercase text-white drop-shadow-2xl">
                   The League
                 </span>
               </h1>
               <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
-                {user ? (
+                <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
                   <Link
-                    to="/notes"
-                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
+                    to="/join"
+                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-amber-700 shadow-sm hover:bg-yellow-50 sm:px-8"
                   >
-                    View Notes for {user.email}
+                    Sign up
                   </Link>
-                ) : (
-                  <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-                    <Link
-                      to="/join"
-                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-amber-700 shadow-sm hover:bg-yellow-50 sm:px-8"
-                    >
-                      Sign up
-                    </Link>
-                    <Link
-                      to="/login"
-                      className="flex items-center justify-center rounded-md bg-amber-300 px-4 py-3 font-medium text-black hover:bg-amber-200  "
-                    >
-                      Log In
-                    </Link>
-                  </div>
-                )}
+                  <Link
+                    to="/login"
+                    className="flex items-center justify-center rounded-md bg-amber-300 px-4 py-3 font-medium text-black hover:bg-amber-200  "
+                  >
+                    Log In
+                  </Link>
+                </div>
               </div>
-                <p className="mx-auto mt-14 max-w-lg text-center text-xl text-white drop-shadow-2xl sm:max-w-3xl">
-                  2022 Season - Time To Draft
-                </p>
+              <p className="mx-auto mt-14 max-w-lg text-center text-xl text-white drop-shadow-2xl sm:max-w-3xl">
+                2022 Season - Time To Draft
+              </p>
             </div>
           </div>
         </div>
