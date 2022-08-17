b6e1b6949fd2495535b291dc7ed34db166294d25
maxwmckinley <max.mck@hey.com>
2022-08-17 14:19:31 -0500

    Initial commit

---
+++ app/routes/posts/$slug.tsx
@@ -0,0 +1,15 @@
+import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader: LoaderFunction = async ({ params }) => {
+  return json({ slug: params.slug });
+};
+
+export default function PostSlug() {
+  const { slug } = useLoaderData();
+  return (
+    <main>
+      <h1>Some Post: {slug}</h1>
+    </main>
+  );
+}
