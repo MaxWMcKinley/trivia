a48fe60a55cc7d9674c59f6f7b2609d8da28a6e6
Ben Ashworth <bashworthj@gmail.com>
2022-08-17 16:40:50 -0400

    im stuck

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        '2xl': '25px 20px 20px rgba(0, 0, 0, 1)'
+    }
   },
   plugins: [],
+}
 };
