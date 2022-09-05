c76918f50837ab5bb69850d728bb3c934afbd25b
bashworthj <105233260+bashworthj@users.noreply.github.com>
2022-09-05 18:09:25 -0400

    worked on quiz page a little (#4)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
     extend: {
       dropShadow: {
         '2xl': '25px 20px 20px rgba(0, 0, 0, 1)'
-    }
+      }
+    },
   },
   plugins: [],
-}
 };
+
+// module.exports = {
+//   theme: {
+//     extend: {
+//       screens: {
+//         'ph': {'raw': '(max-width: 480px)'},
+//       },
+//     }
+//   }
+// }
